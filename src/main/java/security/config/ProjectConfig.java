19801bc6d2786f7ff76810633dbb5cc8cf6af93e
harin1124 <lolrin1124@gmail.com>
2025-12-25 23:41:37 +0900

    refactor: 오버로드된 다른 configure 메소드 이용하여 구현 처리 및 모든 요청에 인증 요구하도록 설정

--- src/main/java/security/config/ProjectConfig.java
+++ src/main/java/security/config/ProjectConfig.java
@@ -1,38 +1,29 @@
 package security.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
-	@Bean
-	public UserDetailsService userDetailsService() {
-		var userDetailService = new InMemoryUserDetailsManager();
-
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		var userDetailsService = new InMemoryUserDetailsManager();
 		var user = User.withUsername("john").password("12345").authorities("read").build();
-		userDetailService.createUser(user);
-
-		return userDetailService;
+		userDetailsService.createUser(user);
+		
+		auth.userDetailsService(userDetailsService)
+				.passwordEncoder(NoOpPasswordEncoder.getInstance());
 	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		// NoOpPasswordEncoder 인스턴스는 암호에 암호화나 해시를 적용하지 않고 일반 텍스트처럼 처리
-		return NoOpPasswordEncoder.getInstance();
-	}
-
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.httpBasic();
-		// 인증 없이 모든 요청 가능
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests().anyRequest().authenticated();
 	}
 }
