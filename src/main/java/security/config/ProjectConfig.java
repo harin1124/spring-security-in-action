7e6e7a0b469341c390765e3462ccedf8e465590d
harin1124 <lolrin1124@gmail.com>
2025-12-26 00:32:21 +0900

    refactor: authenticationProvider 이용

--- src/main/java/security/config/ProjectConfig.java
+++ src/main/java/security/config/ProjectConfig.java
@@ -1,24 +1,20 @@
 package security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private CustomAuthenticationProvider customAuthenticationProvider;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		var userDetailsService = new InMemoryUserDetailsManager();
-		var user = User.withUsername("john").password("12345").authorities("read").build();
-		userDetailsService.createUser(user);
-		
-		auth.userDetailsService(userDetailsService)
-				.passwordEncoder(NoOpPasswordEncoder.getInstance());
+		auth.authenticationProvider(customAuthenticationProvider);
 	}
 	
 	@Override
